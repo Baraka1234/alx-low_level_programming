bfa7206e5e0ab0e60d09186e0ad66c85ddf0f443
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-09-08 11:24:57 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  * Description: the program prints out "Programming is like building a multilingual puzzle
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints string
+ *
+ * Description: using the main function
+ * this program prints "with proper grammer, but the outcome is a piece of art,
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
