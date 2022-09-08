7520afade269f7791644d72c9230922e134fe461
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-09-08 11:04:29 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>>
 
+/**
+ * main - prints the string in the function
+ *
+ * Description: the program prints out "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
