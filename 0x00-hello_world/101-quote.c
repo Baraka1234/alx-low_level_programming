9b54f7aa1376967349920be2324eed1592cd0cc8
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-09-08 16:47:51 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,14 @@
 
 /**
  *
- * main
+ * main: Prints out string
  *
- * Description
+ * Description: printss and that piece of art is useful
  * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar,
-			2015-10-19\n",59);
+	write("and that piece of art is useful -Dora Korpar,
+			2015-10-19\n");
 	return (1);
 }
