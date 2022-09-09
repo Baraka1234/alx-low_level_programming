f97cb685a8e6d22b0bb671b69c204c08cc27f40a
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-09-09 15:59:47 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,13 +3,13 @@
  * main - prints base 10 starting from 0
  *
  * Description: pints from main
- * Returns: 0
+ * Return: 0
  */
 int main(void)
 {
 	int a = 0;
 
-	for (;a < 10 ; a++)
+	for (; a < 10 ; a++)
 	{
 		putchar('0' + a);
 	}
