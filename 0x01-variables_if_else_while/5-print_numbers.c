8133e7492d1e753781a03fdb8f051e3337b4d3bc
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-09-09 15:31:24 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints base 10 starting from zero
+ *
+ * Description: prints frommain
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
