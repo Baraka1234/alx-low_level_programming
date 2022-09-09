0d32053664c212985a87801476534a99138facc3
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-09-09 15:19:37 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase and uppercase alphabets
+ *
+ * Description: Print Lower and uppercase alphas
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
