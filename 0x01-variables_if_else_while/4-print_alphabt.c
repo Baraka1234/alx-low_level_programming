ce76a0f459e0a1ce4d3f0d58652209c59bb98efc
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-09-09 15:26:07 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints alphas in lowercase
+ *
+ * Description: prints from main alphas in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
