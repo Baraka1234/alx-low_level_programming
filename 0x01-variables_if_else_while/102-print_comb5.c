672c18be6fddd0198abf37485709e2f3ba38ed78
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-09-10 15:55:27 -0700

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - prints all combos of two digit numbers
+ *
+ * Description: prints from main all possible two digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + 10);
+			putchar((num2 % 10) + 10);
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			
+			if (num1 == 98 && num2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
