a2bef3bb97ddfd4c041a1912ad6106090243308a
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-09-13 16:03:14 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - function to checke if letter is upper or lowercase
+ * @c: int that checks if letter is uppercase or lowercase
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
