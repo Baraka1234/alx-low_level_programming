dcf99e757c4d0facacc2a483be5c0568cad03554
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-09-13 15:31:45 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - checks for lowercase characters
+ *
+ * Description: a function that checks for lower characters
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char x, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
