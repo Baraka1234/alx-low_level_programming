bea57a0ec9346695246dcc935f18eb23ddc535e6
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-09-13 17:00:49 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - sums two integers
+ * @num1: First integer to be summed
+ * @num2: Second integer to be summed
+ *
+ * Return: the added sum
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2)
+}
