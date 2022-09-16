21beb55c83df1a393e7c1d18d5156cae1ac2a6f9
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-09-15 17:08:38 -0700

    Task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,6 +7,16 @@
 
 void print_number(int n)
 {
-	int num = n;
-	printf( "\d", num);
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		putchar('-');
+		num = -num;
+	}
+	if (num > 9)
+	{
+		print_number(num /10);
+	}
+	putchar(num % 10 + '0');
 }
