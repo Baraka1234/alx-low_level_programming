855fe732db211c388717bf0fb2fac55c5e1a7f9a
Baraka1234 <56594657+Baraka1234@users.noreply.github.com>
2022-09-16 10:11:52 +0100

    Task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- * print_number - Prints number
- * @n: number printed
- */
+* print_number - Prints a number
+* @n: The number to print
+*/
 
 void print_number(int n)
 {
@@ -16,7 +17,7 @@ void print_number(int n)
 	}
 	if (num > 9)
 	{
-		print_number(num /10);
+		print_number(num / 10);
 	}
 	putchar(num % 10 + '0');
 }
