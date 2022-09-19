d1ea0adabb799a42d605c6cf028d62ee931a38fb
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-09-19 14:12:44 -0700

    Completed, without errors?

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy paste string
+ * @dest: destination
+ * @src: source
+ * Return: destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int inc = 0;
+	
+	while (*(src + inc != '\0')
