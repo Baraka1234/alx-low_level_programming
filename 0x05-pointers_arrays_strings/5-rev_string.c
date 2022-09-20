c49678bb23a490d376d5a81e6aa8ae2efeb0b394
Baraka1234 <56594657+Baraka1234@users.noreply.github.com>
2022-09-20 07:26:01 +0100

    5-rev_string.c
    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,8 @@ void rev_string(char *s)
 
 	for(l = 0; s[l] != '\0'; l++)
 
-		for(i = 0; i < l / 2; i++){
+		for(i = 0; i < l / 2; i++)
+		{
 			ch = s[l];
 			s[l] = s[l - 1 - i];
 			s[l - 1 - i] = ch;
