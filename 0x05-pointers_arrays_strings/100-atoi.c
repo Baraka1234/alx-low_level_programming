26aeb6ff956fe49cf95d2d36e693c31bfcab786f
Baraka1234 <56594657+Baraka1234@users.noreply.github.com>
2022-09-20 08:59:33 +0100

    100-atoi.c
    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,28 +2,28 @@
 /**
 * _atoi - Converts a string to an integer
 *
-* @a: Pointer to a character string
+* @s: Pointer to a character string
 * Return: void
 */
 
 int _atoi(char *s)
 {
-  int sign;
-  unsigned int num;
-  char *temp;
+   int sign;
+   unsigned int num;
+   char *temp;
   
-  temp = s;
-  num = 0;
-  sign = 1;
+   temp = s;
+   num = 0;
+   sign = 1;
   
-  while (*temp != '\0' && (*temp < '0' || *temp > '9'))
-  {
-    if (*temp == '-')
-    {
-        sign *= -1;
-    }
-    temp++;
-  }
+   while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+   {
+       if (*temp == '-')
+       {
+           sign *= -1;
+       }
+       temp++;
+   }
         
    if (*temp != '\0')
         {
@@ -35,5 +35,5 @@ int _atoi(char *s)
           while (*temp >= '0' && *temp <= '9');
         }
         return (num * sign);
-  }
+}
         
