3c41af78c5f048803626d98401add5b2499d1e8e
Baraka1234 <56594657+Baraka1234@users.noreply.github.com>
2022-09-21 12:16:45 +0100

    Task 11

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+* infinite_add - adds two numbers
+* @n1: first number to add
+* @n2: second number to add
+* @r: result from addition stored
+* @size_r: size of buffer
+* Return: pointer to the result
+*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
+
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+        int num, tens = 0;
+        
+        for ( ; *n1 && *n2; n1--, n2--, r_index--)
+        {
+                num = (*n1 - '0') + (*n2 - '0');
+                num += tens;
+                *(r + r_index) = (num % 10) + '0';
+                tens = num / 10;
+        }
+        
+        for ( ; *n1; n1--; r_index++)
+        {
+                num = *(n1 - '0') + tens;
+                *(r + r_index) = (num % 10) + '0';
+                tens = num / 10;
+        }
+        
+        for ( ; *n2; n2--; r_index--)
+        {
+                num = (*n2 - '0') + tens;
+                *(r + r_index) = (num % 10) + '0';
+                tens = num / 10;
+        }
+}
