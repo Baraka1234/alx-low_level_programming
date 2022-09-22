2e8a40b8e1e4c3945a3d4b27d0e9bdbe930cf561
Baraka Aminu <56594657+Baraka1234@users.noreply.github.com>
2022-09-22 08:26:30 +0100

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int index , dest_len;
+	
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+	{}
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	for (index = 0; src[index] != '\0'; index++)
+	{
+		dest[dest_len + index] = src[index];
+	}
+	dest[dest_len + index] = '\0';
 
 	return (dest);
 }
