b3424f16f9049489a7295065052148e98ca9b26a
Baraka Aminu <56594657+Baraka1234@users.noreply.github.com>
2022-09-22 19:58:55 +0100

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,44 +1,30 @@
 #include "main.h"
-/**
-* rot13 - function that encodes a string using rot13
-* @str: strings target
-* Return: returns the encoded string
-*/
 
-char *rot13(char *str)
+/**
+ * rot13 - encrypts code
+ * @s: string to encrypt
+ * Return: char value
+ */
+
+char *rot13(char *s)
 {
-        int index1, index2;
-        
-        char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-         'G', 'H', 'I', 'J', 'K', 'L',
-         'M', 'N', 'O', 'P', 'Q', 'R',
-         'S', 'T', 'U', 'V', 'W', 'X',
-         'Y', 'Z', 'a', 'b', 'c', 'd',
-         'e', 'f', 'g', 'h', 'i', 'j',
-         'k', 'l', 'm', 'n', 'o', 'p',
-         'q', 'r', 's', 't', 'u', 'v',
-         'w', 'x', 'y', 'z'};
-         
-        char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-         'T', 'U', 'V', 'W', 'X', 'Y',
-         'Z', 'A', 'B', 'C', 'D', 'E',
-         'F', 'G', 'H', 'I', 'J', 'K',
-         'L', 'M', 'n', 'o', 'p', 'q',
-         'r', 's', 't', 'u', 'v', 'w',
-         'x', 'y', 'z', 'a', 'b', 'c',
-         'd', 'e', 'f', 'g', 'h', 'i',
-         'j', 'k', 'l', 'm'};
-         
-        while (str[++index1])
-        {
-                for (index2 = 0; index2 < 52; index2++)
-                {
-                        if (str[index1] == alphabet[index2])
-                        {
-                                str[index1] = rot13key[index2];
-                                break;
-                        }
-                }
-        }
-        return (str);
+	char part1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char part2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	int i;
+	int j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+		for (j = 0; part1[j] != '\0'; j++)
+		{
+			if (s[i] == part1[j])
+			{
+				s[i] = part2[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
