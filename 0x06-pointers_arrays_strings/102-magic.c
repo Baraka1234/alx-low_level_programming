74e7a5f6e63080b0c55544d022fe279c7127b285
Baraka Aminu <56594657+Baraka1234@users.noreply.github.com>
2022-09-22 21:23:46 +0100

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,28 +1,24 @@
-#include "main.h"
-
-/**
- * _strcat - concats two arrays
- *
- * @dest: destination of concat
- * @src: source array to concat
- *
- * Return: char value
- */
-
-char *_strcat(char *dest, char *src)
-{
-	int i;
-	int j;
-
-	for (j = 0; dest[j] != '\0'; j++)
-	{}
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[j + i] = src[i];
-	}
-	dest[j + i] = '\0';
-
-	return (dest);
-
+#include <stdio.h>                                                                                                                    
+                                                                                                                                      
+int main(void)                                                                                                                        
+{                                                                                                                                     
+        int n;                                                                                                                        
+        int a[5];                                                                                                                     
+        int *p;                                                                                                                       
+                                                                                                                                      
+        a[2] = 1024;                                                                                                                  
+        p = &n;                                                                                                                       
+/*                                                                                                                                    
+ * write your line of code here...                                                                                                    
+ * Remember:                                                                                                                          
+ * main - magic                                                                                                                       
+ * - you are not allowed to use a                                                                                                     
+ *   - you are not allowed to modify p                                                                                                
+ *   - only one statement                                                                                                             
+ *   - you are not allowed to code anything else than this line of code                                                               
+ */                                                                                                                                   
+        *(p + 5) = 98;                                                                                                                
+/* ...so that this prints 98\n */                                                                                                     
+        printf("a[2] = %d\n", a[2]);                                                                                                  
+        return (0);                                                                                                                   
 }
