1d56b78366b3e2c7562cf736568a85762a92d99d
Baraka Aminu <56594657+Baraka1234@users.noreply.github.com>
2022-09-23 01:33:32 +0100

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
- * _strcat - concats two arrays
+ * _strcat - concatenates 2 strings
  *
- * @dest: destination of concat
- * @src: source array to concat
+ * @dest: destination array
+ * @src: source array
  *
- * Return: char value
+ * Return: destination array
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	char *ptr = dest;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (*ptr != 0)
+		ptr++;
+	while (*src != 0)
+		*ptr++ = *src++;
+	*ptr = 0;
 
 	return (dest);
 }
