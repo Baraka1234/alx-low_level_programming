dc5b36c03c1e2f180121f3642a447dfb9a45d135
Baraka Aminu Diyo <baminudeeyo@gmail.com>
2022-10-12 14:41:18 -0700

    Completed tasks

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,8 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function to act on an array of element
+ * @array: an array of element of type int
+ * @size: size of the array
+ * @action: callback function pointing to another function
+ */
