84dbc24a47f84814f7ce0f623bf65635ec21da19
Baraka Aminu <56594657+Baraka1234@users.noreply.github.com>
2022-10-12 22:13:41 +0100

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,11 @@
 #include "function_pointers.h"
+
 /**
- *  * print_name - print a name using function call back
- * * @name: pointer to the name to be printed
- *    * @f: function pointer to call back a function
+ * print_name - print a name using function call back
+ * @name: pointer to the name to be printed
+ * @f: function pointer to call back a function
  * */
+
 void print_name(char *name, void (*f)(char *))
 {
 		if (name == NULL || f == NULL)
