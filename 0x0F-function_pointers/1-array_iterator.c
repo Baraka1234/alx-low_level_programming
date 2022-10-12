8ac1db83da890339469914eed9a955ef9469a7a4
Baraka Aminu <56594657+Baraka1234@users.noreply.github.com>
2022-10-12 22:57:38 +0100

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,3 +6,14 @@
  * @size: size of the array
  * @action: callback function pointing to another function
  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
