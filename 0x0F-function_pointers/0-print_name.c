9d2b189110dd3bc22732941afaa807a73a25b95c
Baraka Aminu <56594657+Baraka1234@users.noreply.github.com>
2022-10-14 03:51:18 +0100

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,21 +1,12 @@
-#include "function_pointers.h"
-
 /**
- * array_iterator - function
- * @array: an array of element
- * @size: size of the array
- * @action: callback function pointing to another function
- *
- * Return: nothing
- */
-
-void array_iterator(int *array, size_t size, void (*action)(int))
+  *print_name - prints a name.
+  *@name: pointer to name.
+  *@f: function pointer.
+  *
+  *Return: void.
+  */
+void print_name(char *name, void (*f)(char *))
 {
-	size_t i;
-
-	if (array == NULL || action == NULL)
-		return;
-
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (name && f)
+		f(name);
 }
